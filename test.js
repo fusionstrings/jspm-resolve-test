dc2f3563031b25a66ec554cc0f37e2116108e239
Glitch (jspm-resolve-test) <none>
2019-12-03 07:57:48 +0000

    🖖🛀 Updated with Glitch

--- test.js
+++ test.js
@@ -1,31 +1,32 @@
-import jspmResolve from '@jspm/resolve';
+import jspmResolve from "@jspm/resolve";
 
 async function testJS() {
-    const { resolved, format } = await jspmResolve(
-        './module.js',
-        process.cwd()
-    );
-    console.log(resolved, format);
+  const { resolved, format } = await jspmResolve("./module.js", process.cwd());
+  console.log(resolved, format);
 }
 
-testJS();
+//testJS();
 
 async function testModuleJS() {
+  try {
     const { resolved, format } = await jspmResolve(
-        './module.mjs',
-        process.cwd()
+      "./module.mjs",
+      process.cwd()
     );
     console.log(resolved, format);
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 testModuleJS();
 
 async function testMissingJS() {
-    const { resolved, format } = await jspmResolve(
-        './missing/module.mjs',
-        process.cwd()
-    );
-    console.log(resolved, format);
+  const { resolved, format } = await jspmResolve(
+    "./missing/module.mjs",
+    process.cwd()
+  );
+  console.log(resolved, format);
 }
 
-testMissingJS();
+//testMissingJS();
